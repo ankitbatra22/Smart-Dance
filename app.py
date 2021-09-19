2d8d00556d98f8925c6af6b68f2fc4e988e1c3e5
ankitbatra2 <ankitbatra2202@gmail.com>
2021-09-19 10:11:40 -0400

    updated

--- app.py
+++ app.py
@@ -35,7 +35,29 @@ mp_hands = mp.solutions.hands
 mp_pose = mp.solutions.pose
 
 
-def connect_points(points, translation_factors, image, image_shape):
+def l2_norm(ground_relative_coords, webcam_relative_coords):
+	return np.linalg.norm(ground_relative_coords - webcam_relative_coords)
+
+def print_data(ground_points, webcam_points, translation_factors, w, h):
+    print(ground_points[str(11)][0:2] * np.array([w, h]) - np.array(list(translation_factors)))
+    print(webcam_points[11][0:2]* np.array([w, h]))
+
+def compare_keypoints(ground_points, webcam_points, w, h, translation_factors):
+	ground_points_array = []
+	webcam_points_array = []
+    
+	for i in range(len(ground_points)):
+		ground_points_array.append(np.array(ground_points[str(i)])[0:2]* np.array([w, h]) - 
+        np.array(list(translation_factors))) # only using x, y coordinates of keypoints
+
+		webcam_points_array.append(np.array(webcam_points[i])[0:2]* np.array([w, h]))
+
+	ground_points_array = np.vstack(ground_points_array)
+	webcam_points_array = np.vstack(webcam_points_array)
+
+	return l2_norm(ground_points_array, webcam_points_array)
+
+def connect_points(points, translation_factors, image, image_shape, scale):
     h, w = image_shape
     points_connect_dict = {
         1: [2, 0],
@@ -66,7 +88,6 @@ def connect_points(points, translation_factors, image, image_shape):
         30: [32],
         32: [28],
     }
-    scale = 1
     for p in points_connect_dict:
         curr_point = points[str(p)][0:2] * np.array([w, h]) - \
             np.array(list(translation_factors))
@@ -76,7 +97,7 @@ def connect_points(points, translation_factors, image, image_shape):
                 np.array(list(translation_factors))
 
             cv2.line(image, (round(curr_point[0] * scale), round(curr_point[1] * scale)),
-                     (round(endpoint[0] * scale), round(endpoint[1] * scale)), (0, 0, 255), thickness=20)
+                     (round(endpoint[0] * scale), round(endpoint[1] * scale)), (0, 0, 255), thickness=10)
 
     return image
 
@@ -94,17 +115,43 @@ def get_translation_factor(gt, person, h, w):
 
 
 def put_text(image, text, h, w):
-    cv2.put_text(h - 50, w - 50, )
-    return None
+    image = cv2.putText(img=image, org=(w - 700, 50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(0, 0, 0), text=text, 
+    thickness= 3)
+    return image
 
 
-def gen_frames(song):
-    with open('metadata/formatted-mustafa-dance-keypoints.json') as f:
+def gen_frames():
+    with open('metadata/' + "formatted-mustafa-dance-keypoints" + '.json') as f:
         data = json.load(f)
 
     counter = 0
     camera = cv2.VideoCapture(0)
-    # infinite webcam loop
+    
+    counter_start = 1
+    max_count = 161
+    while True and counter_start <= (max_count - 1):
+        counter_start  += 1
+        success, image = camera.read()
+        if not success:
+            pass
+        else:
+            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+            h, w, _ = image.shape
+
+            
+            image = cv2.putText(img=image, org=(w//2, h//2), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(0, 0, 0), 
+            text= str((max_count - counter_start)//40), thickness=2)
+       
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            ret, buffer = cv2.imencode('.jpg', image)
+            image = buffer.tobytes()
+            yield (b'--image\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
+
+# infinite webcam loop
+    avg = []
     while True:
         if counter == len(data) - 1:
             counter = 0
@@ -116,23 +163,28 @@ def gen_frames(song):
                 min_tracking_confidence=0.5) as pose:
             # Capture frame-by-frame
             success, image = camera.read()
+
             # print(success.shape)
             if not success:
                 break
             else:
                 image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-
                 groop = image
+
                 image.flags.writeable = False
                 results = pose.process(image)
 
                 pose_landmarks = results.pose_landmarks
                 if pose_landmarks is not None:
-
+                    
+                    scale_t = 1.0
                     h, w, _ = image.shape
-
+                    pose_landmarks_str_keys = {str(i): [lmk.x, lmk.y, lmk.z]
+                                      for i, lmk in enumerate(pose_landmarks.landmark)}
                     pose_landmarks = {i: [lmk.x, lmk.y, lmk.z]
                                       for i, lmk in enumerate(pose_landmarks.landmark)}
+
+                    data[counter] =  {i:[scale_t*coord for coord in data[counter][i]] for i in data[counter]}
 
                     x_t, y_t = get_translation_factor(
                         data[counter], pose_landmarks, h, w)
@@ -151,6 +203,8 @@ def gen_frames(song):
                     # print(results.pose_keypoints_score[0])
 
                     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        
                     mp_drawing.draw_landmarks(
                         image,
                         results.pose_landmarks,
@@ -158,17 +212,30 @@ def gen_frames(song):
                         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
 
                     thingy = connect_points(
-                        data[counter], (x_t, y_t), groop, (h, w))
+                        data[counter], (x_t, y_t), groop, (h, w), scale=1.0)
+                    
+                    thingy = cv2.cvtColor(thingy, cv2.COLOR_RGB2BGR)
+                    thingy = connect_points(pose_landmarks_str_keys, (0, 0), thingy, (h, w), scale=0.1)
 
+                    points = compare_keypoints(data[counter], pose_landmarks, w, h, (x_t, y_t))
+                    
+                    if len(avg) == 20:
+                        avg.pop(0)
+                        avg.append(points)
+                    else:
+                        avg.append(points)
+
+
+                    thingy = put_text(thingy, "Score :" + str(round(sum(avg)/len(avg), 2)), h, w)
                     ret, buffer = cv2.imencode('.jpg', thingy)
                     image = buffer.tobytes()
                     yield (b'--image\r\n'
                            b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
 
-
 @app.route('/dance/<song>')
 def dance(song):
-    # gen_frames(song)
+    global song_name
+    song_name = song
     return render_template('dance.html', song=song)
 
 
@@ -176,7 +243,8 @@ def dance(song):
 def train():
     numDir = len(os.listdir("./metadata"))
     numFiles = os.listdir("./metadata")
-    return render_template('train.html', numDir=numDir, numFiles=numFiles)
+    dances = ["Renegade", "Outwest", "Carlton", "SavageLove", "Floss", "SaySo"]
+    return render_template('dancepage.html', numDir=numDir, numFiles=numFiles, dances=dances)
 
 
 @app.route('/analyze')
@@ -186,7 +254,7 @@ def analyze():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames("song"), mimetype='multipart/x-mixed-replace; boundary=image')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=image')
 
 
 @app.route('/about')
